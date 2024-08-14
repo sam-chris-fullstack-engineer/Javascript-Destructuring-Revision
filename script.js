f72c620c08165e93826476a1d315c0b62d846659
sam-christopher07 <samchristophergit07@gmail.com>
2024-08-14 21:11:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//Array Destructuring arrays
+
+//without Destructuring:
+const arr = [2, 3, 4];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+console.log(a, b, c);
+
+//with Destructuring:
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// const [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+// const [third, ,fourth] = restaurant.categories;
+// console.log(third, fourth);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//Switching variables without Destructuring:
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+//Switching variables with Destructuring:
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
